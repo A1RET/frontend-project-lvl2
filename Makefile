1ef0735b4702751bdc473e218ab504c635a60a7a
Airet <thiefon@gmail.com>
2019-10-06 13:52:21 +0500

    первый шаг

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 install: 
 	npm install
 
+start:
+	npx babel-node src/bin/gendiff.js
+
+build:
+	npm run build
+
 publish: 
 	npm publish --dry-run
 
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+import commander from 'commander';
+
+const program = commander;
+
+program
+  .arguments('')
+  .version('1.0')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv)
