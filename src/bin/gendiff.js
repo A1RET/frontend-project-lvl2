7d9c7031f8e805ccecf5215054992d40ab932f8d
Airet <thiefon@gmail.com>
2019-10-13 12:21:01 +0500

    step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,6 +6,8 @@ const program = commander;
 program
   .arguments('')
   .version('1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>');
 
 program.parse(process.argv);
