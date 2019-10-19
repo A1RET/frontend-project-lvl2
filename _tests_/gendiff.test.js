c6bd2579b79bc7ff55b3e5f672770c4a749b96b3
Airet <thiefon@gmail.com>
2019-10-19 11:58:52 +0500

    плагин линтера для jest

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -3,9 +3,9 @@ import fs from 'fs';
 import gendiff from '../src';
 
 test('gendiff', () => {
-  const before = path.resolve(__dirname, `_fixtures_/before_plain.json`);
-  const after = path.resolve(__dirname, `_fixtures_/after_plain.json`);
-  const result = fs.readFileSync(path.resolve(__dirname, `_fixtures_/result_plain.txt`), 'utf8');
+  const before = path.resolve(__dirname, '_fixtures_/before_plain.json');
+  const after = path.resolve(__dirname, '_fixtures_/after_plain.json');
+  const result = fs.readFileSync(path.resolve(__dirname, '_fixtures_/result_plain.txt'), 'utf8');
 
   expect(gendiff(before, after)).toBe(result);
 });
