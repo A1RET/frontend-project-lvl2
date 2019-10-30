bea4e9d21ce5365318bc362aff6a67188c675087
Roman Mironov <thiefon@gmail.com>
2019-10-30 18:03:06 +0500

    тесты на сравнение yml step5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,9 +3,14 @@ import fs from 'fs';
 import gendiff from '../src';
 
 test('gendiff', () => {
-  const before = path.resolve(__dirname, '__fixtures__/before_plain.json');
-  const after = path.resolve(__dirname, '__fixtures__/after_plain.json');
+  const beforeJson = path.resolve(__dirname, '__fixtures__/before_plain.json');
+  const afterJson = path.resolve(__dirname, '__fixtures__/after_plain.json');
+
+  const beforeYaml = path.resolve(__dirname, '__fixtures__/before_plain.yml');
+  const afterYaml = path.resolve(__dirname, '__fixtures__/after_plain.yml');
+
   const result = fs.readFileSync(path.resolve(__dirname, '__fixtures__/result_plain.txt'), 'utf8');
 
-  expect(gendiff(before, after)).toBe(result);
+  expect(gendiff(beforeJson, afterJson)).toBe(result);
+  expect(gendiff(beforeYaml, afterYaml)).toBe(result);
 });
