ae1de6a11f9a58865bfc5a66edab39a702db9604
Roman Mironov <thiefon@gmail.com>
2019-11-25 13:18:34 +0500

    вывод в json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import recursive from './recursive';
 import plain from './plain';
+import json from './json';
 
 const formatters = {
   recursive,
   plain,
+  json,
 };
 
 export default (ast, format) => formatters[format](ast);
