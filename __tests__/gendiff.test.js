de98b144f5b3e1083e5a862ebec7bea831a2aa92
Roman Mironov <thiefon@gmail.com>
2019-11-25 13:18:11 +0500

    тесты для формата json

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,6 +7,11 @@ test.each([
   ['before.yml', 'after.yml', 'recursive'],
   ['before.ini', 'after.ini', 'recursive'],
   ['before.json', 'after.json', 'plain'],
+  ['before.yml', 'after.yml', 'plain'],
+  ['before.ini', 'after.ini', 'plain'],
+  ['before.json', 'after.json', 'json'],
+  ['before.yml', 'after.yml', 'json'],
+  ['before.ini', 'after.ini', 'json'],
 ])('gendiff', (before, after, format) => {
   const beforeFile = path.resolve(__dirname, `__fixtures__/${before}`);
   const afterFile = path.resolve(__dirname, `__fixtures__/${after}`);
